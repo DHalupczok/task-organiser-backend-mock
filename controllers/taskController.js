72ef504c09cc07ff2114d2ec514a7fd153e2ea7d
Denis Halupczok <denis.halupczok.external@atos.net>
2023-08-28 07:10:12 +0200

    isDone property added, other minor improvements

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,49 +1,65 @@
-const Task = require('../models/taskModel')
+const Task = require("../models/taskModel");
 
 exports.getAllTasks = (req, res) => {
-    const projectId = req.params.projectId;
-    const responseData = Task.getAllTasksByProjectId(projectId);
-    res.status(200);
-    res.send(responseData);
-}
+  const projectId = req.params.projectId;
+  const responseData = Task.getAllTasksByProjectId(projectId);
+  res.status(200);
+  res.send(responseData);
+};
 
 exports.createNewTask = (req, res) => {
-    const newTask = new Task(req.body.projectId, req.body.title, req.body.startDate, req.body.stopDate, req.body.repeatability, req.body.content);
-    newTask.save();
-    const responseData = Task.getAllTasksByProjectId(newTask.projectId);
-    res.status(200);
-    res.send(responseData);
-}
+  const newTask = new Task(
+    req.body.projectId,
+    req.body.title,
+    req.body.startDate,
+    req.body.stopDate,
+    req.body.repeatability,
+    req.body.content
+  );
+  newTask.save();
+  const responseData = Task.getAllTasksByProjectId(newTask.projectId);
+  res.status(200);
+  res.send(responseData);
+};
 
 exports.editExistingTask = (req, res) => {
-    const taskId = req.body.id;
+  const taskId = req.body.id;
 
-    if (!taskId) {
-        res.status(404).send()
-        return
-    }
-    const updateAnswer = Task.updateTaskById(taskId, req.body.projectId, req.body.title, req.body.startDate, req.body.stopDate, req.body.repeatability, req.body.content);
-    if (!updateAnswer) {
-        res.status(404).send()
-        return;
-    }
-    const responseData = Task.getAllTasksByProjectId(req.body.projectId);
-    res.status(200);
-    res.send(responseData);
-}
+  if (!taskId) {
+    res.status(404).send();
+    return;
+  }
+  const updateAnswer = Task.updateTaskById(
+    taskId,
+    req.body.projectId,
+    req.body.title,
+    req.body.startDate,
+    req.body.stopDate,
+    req.body.repeatability,
+    req.body.content,
+    req.body.isDone
+  );
+  if (!updateAnswer) {
+    res.status(404).send();
+    return;
+  }
+  const responseData = Task.getAllTasksByProjectId(req.body.projectId);
+  res.status(200);
+  res.send(responseData);
+};
 
 exports.deleteExistingTask = (req, res) => {
-    const taskId = req.params.taskId;
-    if (!taskId) {
-        res.status(404).send();
-        return;
-    }
-    const task = Task.getTaskById(taskId);
-    if (!task) {
-        res.status(404).send();
-        return;
-    }
-    Task.delete(taskId);
-    res.status(200);
-    res.send();
-}
+  const taskId = req.params.taskId;
+  if (!taskId) {
+    res.status(404).send();
+    return;
+  }
+  const task = Task.getTaskById(taskId);
+  if (!task) {
+    res.status(404).send();
+    return;
+  }
+  Task.delete(taskId);
+  res.status(200);
+  res.send();
+};
--- models/taskModel.js
+++ models/taskModel.js
@@ -1,70 +1,94 @@
 //	{id?: string, title: string, startDate: Date, stopDate: Date, repeatability: ERepeatability, content: string }
 
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const tasks = [{
-    id: '1',
-    projectId: '1',
-    typeId: '1',
-    title: 'Daily meeting',
+const tasks = [
+  {
+    id: "1",
+    projectId: "1",
+    typeId: "1",
+    title: "Daily meeting",
     startDate: new Date(2023, 2, 1, 9, 0),
     stopDate: new Date(2023, 2, 28, 9, 30),
     repeatability: 1,
-    content: 'daily of our team'
-}, {
-    id: '2',
-    projectId: '1',
-    typeId: '1',
-    title: 'Sprint review meeting',
+    content: "daily of our team",
+    isDone: false,
+  },
+  {
+    id: "2",
+    projectId: "1",
+    typeId: "1",
+    title: "Sprint review meeting",
     startDate: new Date(2023, 3, 3, 10, 0),
     stopDate: new Date(2023, 3, 31, 11, 30),
     repeatability: 2,
-    content: 'Retro'
-}, {
-    id: '3',
-    projectId: '2',
-    typeId: '1',
-    title: 'Organise retro',
+    content: "Retro",
+    isDone: false,
+  },
+  {
+    id: "3",
+    projectId: "2",
+    typeId: "1",
+    title: "Organise retro",
     startDate: new Date(2023, 2, 3, 9, 30),
-    stopDate: new Date(2023, 2, 28, 10,),
+    stopDate: new Date(2023, 2, 28, 10),
     repeatability: 2,
-    content: 'Retro of our team'
-}]
-
+    content: "Retro of our team",
+    isDone: true,
+  },
+];
 
 module.exports = class Task {
-    constructor(projectId, title, startDate, stopDate, repeatability, content) {
-        this.id = uuidv4();
-        this.projectId = projectId;
-        this.title = title;
-        this.startDate = startDate;
-        this.stopDate = stopDate;
-        this.repeatability = repeatability;
-        this.content = content;
-    }
+  constructor(projectId, title, startDate, stopDate, repeatability, content) {
+    this.id = uuidv4();
+    this.projectId = projectId;
+    this.title = title;
+    this.startDate = startDate;
+    this.stopDate = stopDate;
+    this.repeatability = repeatability;
+    this.content = content;
+    this.isDone = false;
+  }
 
-    static getAllTasksByProjectId(id) {
-        return tasks.filter(task => task.projectId === id);
-    }
+  static getAllTasksByProjectId(id) {
+    return tasks.filter((task) => task.projectId === id);
+  }
 
-    static updateTaskById(id, projectId, title, startDate, stopDate, repeatability, content) {
-        const index = tasks.findIndex(el => el.id === id);
-        if(index === -1) return false;
-        tasks[index] = {...tasks[index], title, startDate, stopDate, repeatability, content};
-        return tasks[index]
-    }
+  static updateTaskById(
+    id,
+    projectId,
+    title,
+    startDate,
+    stopDate,
+    repeatability,
+    content,
+    isDone
+  ) {
+    const index = tasks.findIndex((el) => el.id === id);
+    if (index === -1) return false;
+    tasks[index] = {
+      ...tasks[index],
+      projectId,
+      title,
+      startDate,
+      stopDate,
+      repeatability,
+      content,
+      isDone,
+    };
+    return tasks[index];
+  }
 
-    static delete(id) {
-        const index = tasks.findIndex(el => el.id === id)
-        tasks.splice(index, 1);
-    }
+  static delete(id) {
+    const index = tasks.findIndex((el) => el.id === id);
+    tasks.splice(index, 1);
+  }
 
-    static getTaskById(taskId) {
-        return tasks.find(task => task.id === taskId)
-    }
+  static getTaskById(taskId) {
+    return tasks.find((task) => task.id === taskId);
+  }
 
-    save() {
-        tasks.push(this);
-    }
-}
-
+  save() {
+    tasks.push(this);
+  }
+};
